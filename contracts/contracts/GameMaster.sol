b4ab2c398becd37af163910e745b3e93104eff81
Falco90 <falcorodenburg@gmail.com>
2023-10-18 10:19:03 +0700

    modify contracts to be AxelarExecutable

--- contracts/contracts/GameMaster.sol
+++ contracts/contracts/GameMaster.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract GameMaster {
+import {AxelarExecutable} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import {IAxelarGateway} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
+import {IAxelarGasService} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import {IERC20} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol";
+
+contract GameMaster is AxelarExecutable {
+    string public value;
+    string public sourceChain;
+    string public sourceAddress;
+    IAxelarGasService public immutable gasService;
+
     mapping(uint => Item) public items;
 
     struct Item {
@@ -16,6 +26,13 @@ contract GameMaster {
         uint dexterity;
         uint charisma;
         uint constitution;
+    }
+
+    constructor(
+        address gateway_,
+        address gasReceiver_
+    ) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasReceiver_);
     }
 
     function createItem(string calldata _name) public returns (Item memory) {}
--- contracts/contracts/Player.sol
+++ contracts/contracts/Player.sol
@@ -1,13 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Player {
+import {AxelarExecutable} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import {IAxelarGateway} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol";
+import {IAxelarGasService} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import {IERC20} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol";
+
+contract Player is AxelarExecutable {
+    string public value;
+    string public sourceChain;
+    string public sourceAddress;
+    IAxelarGasService public immutable gasService;
+    
     mapping(bytes => Character) characters;
 
     struct Character {
-       Attributes attributes;
-       Avatar avatar;
-       Inventory inventory;
+        Attributes attributes;
+        Avatar avatar;
+        Inventory inventory;
     }
 
     struct Attributes {
@@ -28,16 +38,29 @@ contract Player {
         uint tokenId;
     }
 
-    modifier OnlyAvatarOwner {
+    modifier OnlyAvatarOwner() {
         // Only the owner of the Avatar(ERC721 on L1) can call functions with this modifier
         // Read from L1 contract through Axelar / Scroll Messenger
         // require(assetOwner == msg.sender)
         _;
     }
 
-    function createCharacter(Avatar calldata _avatar, Attributes calldata _attributes) public {
-        bytes memory id = abi.encodePacked(_avatar.contractAddress, _avatar.tokenId);
-        
+    constructor(
+        address gateway_,
+        address gasReceiver_
+    ) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasReceiver_);
+    }
+
+    function createCharacter(
+        Avatar calldata _avatar,
+        Attributes calldata _attributes
+    ) public {
+        bytes memory id = abi.encodePacked(
+            _avatar.contractAddress,
+            _avatar.tokenId
+        );
+
         Inventory memory inventory;
         characters[id] = Character(_attributes, _avatar, inventory);
     }
